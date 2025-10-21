f9804452d1b3e48f364609c77b26af37f3310b2f
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-10-21 15:07:58 +0700

    Implementasi Endpoint Create (POST)

--- index.js
+++ index.js
@@ -28,3 +28,34 @@ db.connect((err) => {
   }
   console.log('Koneksi Berhasil!');
 });
+
+app.get('/api/mahasiswa', (req, res) => {
+  db.query('SELECT * from biodata', (err, results) => {
+    if (err) {
+      console.error('Error executing query:' + err.stack);
+      res.status(500).send('Error fetching users');
+      return;
+    }
+    res.json(results);
+  });
+});
+
+app.post('/api/mahasiswa', (req, res) => {
+  const {nama, alamat, agama} = req.body;
+
+  if (!nama || !alamat || !agama) {
+    return res.status(400).json({ message: "Nama, alamat, dan agama harus diisi."});
+  }
+
+  db.query(
+    'INSERT INTO biodata (nama, alamat, agama) VALUES (?, ?, ?)',
+    [nama, alamat, agama],
+    (err, results) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ message: "Database Error"});
+      }
+      res.status(201).json({ message: "User created successfully"});
+    }
+  );
+});
